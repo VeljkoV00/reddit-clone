d8e6e0ffd8f5dab8431aa92aa4da108a0616f58d
VeljkoV00 <veljkovelimirovic7@gmail.com>
2022-11-12 14:57:38 +0100

    Added pagination

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex">
+    <template v-for="(link, key) in links" :key="key">
+        <div class="mb-1 mr-1 px-4 py-3 text-gray-400 leading-4 border rounded"
+        v-if="link.url ===null" v-html="link.label" >
+            
+        </div>
+        <Link v-else :href="link.url" v-html="link.label" class="mb-1 mr-1 px-4 py-3 text-gray-900 leading-4 border rounded hover:bg-slate-600
+        focus:text-indigo-500
+        focus:border-indigo-700
+        "></Link>
+    </template>
+  </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+defineProps({
+    links: []
+})
+
+</script>
+
+<style>
+
+</style>
--- resources/js/Pages/Community/Index.vue
+++ resources/js/Pages/Community/Index.vue
@@ -4,10 +4,11 @@ import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
+import Pagination from "@/Components/Pagination.vue" 
 import { Head, Link } from "@inertiajs/inertia-vue3";
 
 defineProps({
-  communities: Object
+  communities: Object,
 });
 </script>
 
@@ -23,9 +24,21 @@ defineProps({
 
     <div class="flex justify-end">
       <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-        <Link :href="route('communities.create')" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600">Create</Link>
+        <Link
+          :href="route('communities.create')"
+          class="
+            inline-flex
+            items-center
+            justify-center
+            rounded-md
+            border border-transparent
+            bg-indigo-600
+          "
+          >Create</Link
+        >
       </div>
     </div>
+
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="max-w-md mx-auto bg-slate-100 m-2 p-2">
@@ -64,15 +77,44 @@ defineProps({
               </tr>
             </thead>
             <tbody class="divide-y divide-gray-200 bg-white">
-              <tr v-for="community in communities" v-bind:key="community.id">
-                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-smfont-medium text-gray-900 sm:pl-6">{{ community.name }}</td>
-                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ community.slug }}</td>
+              <tr v-for="community in communities.data" v-bind:key="community.id">
+                <td
+                  class="
+                    whitespace-nowrap
+                    py-4
+                    pl-4
+                    pr-3
+                    text-smfont-medium text-gray-900
+                    sm:pl-6
+                  "
+                >
+                  {{ community.name }}
+                </td>
                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                  <Link :href="route('communities.edit', community.id)" class="text-indigo-600 hover:text-indigo-900">Edit</Link>:
+                  {{ community.slug }}
+                </td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                  <Link
+                    :href="route('communities.edit', community.id)"
+                    class="text-indigo-600 hover:text-indigo-900 mr-3"
+                    >Edit</Link
+                  >
+                  <Link
+                    :href="route('communities.destroy', community.id)"
+                    class="text-indigo-600 hover:text-red-600"
+                    method="delete"
+                    as="button"
+                    type="button"
+                    >Delete</Link
+                  >
                 </td>
               </tr>
             </tbody>
           </table>
+          <div class="m-2 p-2">
+            <Pagination :links="communities.links"></Pagination>
+
+          </div>
         </div>
       </div>
     </div>
